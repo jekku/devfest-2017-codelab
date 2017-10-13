d26092d785c6e3c4883fbdd0503c5ed14f1ed846
jekku <jekripreclaroorlina@gmail.com>
2017-10-14 00:55:26 +0800

    Remove unnecessaries

--- src/map-samples.spec.js
+++
@@ -1,5 +0,0 @@
-import { doubledValues } from './map-samples'
-
-test('Should double all the values in the array', () => {
-  expect(doubledValues([3, 6, 9])).toEqual([6, 12, 18])
-});
--- src/reduce-samples.spec.js
+++
@@ -1,6 +0,0 @@
-import { rowMultiplication } from './reduce-samples'
-import { numbers } from './data'
-
-test('Should get total of the row multiplied', () => {
-  expect(rowMultiplication(numbers)).toBe(24)
-})
