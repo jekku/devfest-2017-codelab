aaf537f9d4bf8508ea358c27afaae45e3524e67f
Humphrey <humphrey.litan@gmail.com>
2018-07-03 17:44:13 +0800

    modified js samples

--- src/map-samples.js
+++ src/map-samples.js
@@ -3,9 +3,13 @@
 export const doubledValues = (numbers) => {
   const result = []
 
-  for (let i = 0; i < numbers.length; i++) {
+  /* for (let i = 0; i < numbers.length; i++) {
     result.push(numbers[i] * 2)
-  }
+  } */
+
+  numbers.map((value) => {
+    result.push(value * 2)
+  });
 
   return result
 }
