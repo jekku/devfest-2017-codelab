089ca1e4455136215a846830805abf7d62142e02
Joshua <niputen13@gmail.com>
2018-07-10 13:02:26 +0800

    added counting dog breed function

--- src/reduce-samples.js
+++ src/reduce-samples.js
@@ -6,9 +6,20 @@ export const rowMultiplication = (row) => {
   /*for (let i = 0; i < row.length; i++) {
     result = result * row[i]
   }*/
-  
+
   const reducer = (result, row) =>  result * row;
   result = row.reduce(reducer);
 
   return result;
+}
+
+export const countDogBreed = (dogs) => {
+  return dogs.reduce((count, dog)=> {
+      if(count[dog.breed]){
+        count[dog.breed] = count[dog.breed] + 1
+      }else {
+        count[dog.breed] = 1
+      }
+      return count
+    },{})
 }
